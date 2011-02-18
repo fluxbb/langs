9d12d8027fc09a588e5080a895023d32819bd97a
SiaNariman <sia.nariman@gmail.com>
2011-02-18 23:03:59 +0100

    modified:   userlist.php

--- Kurdish/userlist.php
+++ Kurdish/userlist.php
@@ -6,7 +6,7 @@ $lang_ul = array(
 'User find legend'	=>	'دۆزینەوە و جیاکردنەوەی بەکارهێنەران',
 'User search info'	=>	'Enter a username to search for and/or a user group to filter by. The username field can be left blank. Use the wildcard character * for partial matches.',
 'User sort info'	=>	'Sort users by name, date registered or number of posts and in ascending/descending order.',
-'User group'		=>	'User group',
+'User group'		=>	'گرووپی بەکارهێنەران',
 'No of posts'		=>	'Number of posts',
 'All users'			=>	'هەموو'
 
