fcbdc3cc3cd76ea6eedc562b4fa37a8540ccca9a
Mahdi <mahdiiiiiii@gmail.com>
2013-02-26 22:13:06 +0330

    fix

--- Persian/functions.php
+++
@@ -1,9 +0,0 @@
-﻿<?php
-
-// Language definitions used in install.php, localized by adaur
-
-$lang_install = array(
-
-'An error was encountered'				=>	'مدیران سایت',
-
-}
--- Persian/funnyquestion.php
+++
@@ -1,9 +0,0 @@
-<?php
-
-$lang_funnyquestion = array(
-	'question-label' => 'پاسخ شما',
-	'wrong-answer' => 'متاسفانه پاسخ شما اشتباه است. دوباره تلاش کنید.!'
-);
-
-?>
-
