e6edbbbc0e8c61dcac6f4dd84c49c06e9e737f4d
Oscon <info@oscon.it>
2015-03-08 22:05:10 +0100

    minor improvements

--- Italian/index.php
+++ Italian/index.php
@@ -6,15 +6,15 @@
 $lang_index = array(
 
 'Topics'		=>	'Discussioni',
-'Link to'		=>	'Collegamento a:', // Es: "Collegamento a: http://fluxbb.org/"
-'Empty board'	=>	'Forum vuoto!',
-'Newest user'	=>	'Benvenuto a: %s !', // Utente più recente:
+'Link to'		=>	'Collegamento a:', // Esempio: "Collegamento a: http://fluxbb.org/"
+'Empty board'	=>	'Forum vuoto.',
+'Newest user'	=>	'Benvenuto a: %s !', // Utente piÃ¹ recente
 'Users online'	=>	'Utenti in linea: %s',
 'Guests online'	=>	'Ospiti in linea: %s',
 'No of users'	=>	'Utenti totali: %s',
 'No of topics'	=>	'Discussioni totali: %s',
 'No of posts'	=>	'Messaggi totali: %s',
-'Online'		=>	'In linea:', // Es: "In linea: Pippo, Pluto, Paperino", ecc.
+'Online'		=>	'In linea:', // Esempio: "In linea: Pippo, Pluto, Paperino [...]".
 'Board info'	=>	'Informazioni forum',
 'Board stats'	=>	'Statistiche forum',
 'User info'		=>	'Informazioni utente'
