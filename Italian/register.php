b267ca69f9675a4b6d93623b4b864a9aa9696df1
Oscon <info@oscon.it>
2015-03-08 22:40:25 +0100

    minor improvements

--- Italian/register.php
+++ Italian/register.php
@@ -9,8 +9,8 @@ $lang_register = array(
 'No new regs'				=>	'Il forum non accetta nuove registrazioni.',
 'Reg cancel redirect'		=>	'Registrazione cancellata. Reindirizzamento &hellip;',
 'Forum rules'				=>	'Regole del forum',
-'Rules legend'				=>	'Accettare quanto segue per registrarsi',
-'Registration flood'		=>	'Un utente col tuo stesso indirizzo IP si &egrave; registrato negli ultimi minuti. Per prevenire abusi &egrave; necessario riprovare pi&ugrave; tardi, grazie.',
+'Rules legend'				=>	'Accettare le regole seguenti per registrarsi',
+'Registration flood'		=>	'Un utente col tuo stesso indirizzo IP si &egrave; registrato negli ultimi minuti. Per prevenire abusi &egrave; necessario cambiare il proprio IP o riprovare pi&ugrave; tardi, grazie.',
 'Agree'						=>	'Accetto',
 'Cancel'					=>	'Cancella',
 'Register'					=>	'Registrati',
@@ -18,7 +18,7 @@ $lang_register = array(
 // Validazione moduli (alcune usate anche in post.php)
 'Registration errors'		=>	'Errori di registrazione',
 'Registration errors info'	=>	'Prima di registrarsi &egrave; necessario correggere i seguenti errori:',
-'Username censor'			=>	'Il nome utente utilizza una o pi&ugrave; parole vietate. Scegline un altro, grazie.',
+'Username censor'			=>	'Il nome utente utilizza una o pi&ugrave; parole non permesse. Scegline un altro, grazie.',
 'Username dupe 1'			=>	'Il nome utente &egrave; gi&agrave; utilizzato.',
 'Username dupe 2'			=>	'I nomi utente devono differire tra loro almeno di un carattere (a-z o 0-9). Scegline un altro, grazie.',
 'Email not match'			=>	'Gli indirizzi email non corrispondono.',
@@ -28,8 +28,8 @@ $lang_register = array(
 'Reg complete'				=>	'Registrazione completata. Connessione in corso &hellip;',
 
 // Informazioni di registrazione
-'Desc 1'					=>	'La registrazione d&agrave; accesso a funzioni aggiuntive, tra queste: la possibilit&agrave; di modificare e cancellare i messaggi, inserire una firma personalizzata, un avatar e molto altro.',
-'Desc 2'					=>	'Per registrarsi compilare il modulo. Una volta effettuata la registrazione &egrave; possibile accedere alle personalizzazioni avanzate dal profilo utente.',
+'Desc 1'					=>	'La registrazione d&agrave; accesso a funzioni aggiuntive, tra queste: la possibilit&agrave; di modificare i messaggi, inserire una firma personalizzata, un avatar e molto altro.',
+'Desc 2'					=>	'Per registrarsi compilare il modulo. Una volta effettuata la registrazione &egrave; possibile accedere alle personalizzazioni avanzate dal proprio profilo utente.',
 'Username legend'			=>	'Inserire un nome utente (minimo 2 e massimo 25 caratteri)',
 'Pass legend'				=>	'Inserire e confermare una password (minimo 6 caratteri)',
 'Pass info'					=>	'La password deve avere una lunghezza minima di 6 caratteri ed &egrave; sensibile alle maiuscole (<em>case sensitive</em>).',
