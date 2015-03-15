ff99d8adaf0de6d1f1a472f2a9b71e75071ae21d
Olexandr Neznaiko <opalj.007@gmail.com>
2015-03-15 16:06:57 +0200

    Ukrainian translation updated to version 1.5.8

--- Ukrainian/admin_reports.php
+++ Ukrainian/admin_reports.php
@@ -3,19 +3,19 @@
 // Language definitions used in admin_reports.php
 $lang_admin_reports = array(
 
-'Report zapped redirect'	=>	'Скаргу розглянуто. Переадресація …',
-'New reports head'			=>	'Нові скарги',
-'Deleted user'				=>	'Користувач видалений',
-'Deleted'					=>	'Видалено',
-'Post ID'					=>	'Повідомлення #%s',
-'Report subhead'			=>	'Скаргу подано %s',
-'Reported by'				=>	'Поскаржився %s',
-'Reason'					=>	'Причина',
-'Zap'						=>	'Розглянуто',
-'No new reports'			=>	'Немає нових скарг.',
-'Last 10 head'				=>	'Останні 10 з уже розглянутих',
-'NA'						=>	'Н/Д',
-'Zapped subhead'			=>	'Розглянуто %s %s',
-'No zapped reports'			=>	'Поки немає розглянутих сигналів.',
+'Report zapped redirect'		=>		'Скаргу розглянуто. Переадресація …',	
+'New reports head'		=>		'Нові скарги',	
+'Deleted user'		=>		'Користувач видалений',	
+'Deleted'		=>		'Видалено',	
+'Post ID'		=>		'Повідомлення #%s',	
+'Report subhead'		=>		'Скаргу подано %s',	
+'Reported by'		=>		'Поскаржився %s',	
+'Reason'		=>		'Причина',	
+'Zap'		=>		'Розглянуто',	
+'No new reports'		=>		'Немає нових скарг.',	
+'Last 10 head'		=>		'Останні 10 з уже розглянутих',	
+'NA'		=>		'Н/Д',	
+'Zapped subhead'		=>		'Розглянуто %s %s',	
+'No zapped reports'		=>		'Поки немає розглянутих сигналів.',	
 
 );
