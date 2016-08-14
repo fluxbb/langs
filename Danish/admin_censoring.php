16d879e1d2c9602cc4e43ed34b4b628138eea88b
34626 <kenneth.90rasmussen@gmail.com>
2016-08-14 13:07:27 +0200

    updated admin_censoring.php
    minor translations.. and word adjustments..

--- Danish/admin_censoring.php
+++ Danish/admin_censoring.php
@@ -3,11 +3,11 @@
 // Language definitions used in admin_censoring.php
 $lang_admin_censoring = array(
 
-'Must enter word message'	=>	'Du skal indtaste et ord du ønsker at censurere.',
-'Word updated redirect'		=>	'Censureret ord opdateret. Omdirigere …',
-'Word added redirect'		=>	'Censureret ord tilføjet. Omdirigere …',
-'Word removed redirect'		=>	'Censureret ord fjernet. Omdirigere …',
-'Censoring head'			=>	'Censurere',
+'Must enter word message'	=>	'Du skal angive ordet du ønsker at censurerer.',
+'Word updated redirect'		=>	'Det angivne ord blev censureret.. Omdirigerer …',
+'Word added redirect'		=>	'Censureret ord tilføjet. Omdirigerer …',
+'Word removed redirect'		=>	'Censureret ord fjernet. Omdirigerer …',
+'Censoring head'			=>	'Censurering',
 'Add word subhead'			=>	'Tilføj ord',
 'Add word info'				=>	'Indtast et ord du ønsker at censurere og en erstatningstekst for det valgte ord. Wildcards er tilladt (f.eks. *vin* ville matche hvidvin og rødvin). Censur af ord påvirker også brugernavne. Nye brugere vil ikke være istand til at registrere med brugernavne som indeholder nogle af de censurerede ord. Søgningen tager ikke højde for store og små bogstaver.',
 'Censoring enabled'			=>	'<strong>Censurering er aktiveret i %s.</strong>',
@@ -19,3 +19,4 @@ $lang_admin_censoring = array(
 'No words in list'			=>	'Ingen censur ord i listen.',
 
 );
+aaaaaa
