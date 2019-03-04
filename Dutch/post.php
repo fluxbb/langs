ff8ffdfd4f2ca0eaba2d5820ead050d2995ac4e8
fp10Ho <48217340+fp10Ho@users.noreply.github.com>
2019-03-04 18:53:33 +0100

    Update post.php
    Some different wording and some grammar correction

--- Dutch/post.php
+++ Dutch/post.php
@@ -1,25 +1,28 @@
 <?php
 
 // Taaldefinities gebruikt in post.php en edit.php
+
 $lang_post = array(
 
 // Bericht valideren
-'No subject'		=>	'Je moet een onderwerp opgeven.',
-'No subject after censoring'	=>	'Je moet een onderwerp opgeven. Door de filter werd je onderwerp verwijderd.',
-'Too long subject'	=>	'Onderwerp mag maximum 70 karakters bevatten.',
+
+'No subject'		=>	'Vul een onderwerp in.',
+'No subject after censoring'	=>	'Vul een ander onderwerp in. Door de filter werd je onderwerp verwijderd.',
+'Too long subject'	=>	'Onderwerp mag maximaal 70 karakters bevatten.',
 'No message'		=>	'Je moet een bericht opgeven.',
 'No message after censoring'	=>	'Je moet een bericht opgeven. Door de filter werd je bericht verwijderd',
 'Too long message'	=>	'Je bericht mag niet langer zijn dan %s bytes.',
 'All caps subject'	=>	'Onderwerp kan niet bestaan uit enkel hoofdletters.',
-'All caps message'	=>	'Bericht kan niet bestaan uit enkel hoofdletters..',
+'All caps message'	=>	'Bericht kan niet bestaan uit enkel hoofdletters.',
 'Empty after strip'	=>	'Het lijkt erop dat je post enkel bestaat uit BBCodes. Dit kan gebeuren als je citaten gebruikt die het diepte niveau overschrijden',
 
 // Posten
+
 'Post errors'		=>	'Post errors',
 'Post errors info'	=>	'De volgende problemen moeten worden opgelost voordat je post kan worden gepubliceerd:',
 'Post preview'		=>	'Toon voorbeeld',
 'Guest name'		=>	'Naam', // Enkel voor gasten
-'Post redirect'		=>	'Post ingevoerd. Doorverwijzen …',
+'Post redirect'		=>	'Post ingevoerd. Doorverwijzen…',
 'Post a reply'		=>	'Reageren',
 'Post new topic'	=>	'Nieuw topic',
 'Hide smilies'		=>	'Gebruik nooit emoticons in deze post',
@@ -31,9 +34,11 @@ $lang_post = array(
 'Preview'			=>	'Voorbeeld', // Voorbeeld voor bericht
 
 // Post bewerken
+
 'Edit post legend'	=>	'Bewerken en verzenden',
 'Silent edit'		=>	'Stil bewerken (weergeef "Bewerkt door ..." niet in dit topic)',
 'Edit post'			=>	'Bewerk post',
-'Edit redirect'		=>	'Post bijgewerkt. Doorverwijzen …'
+'Edit redirect'		=>	'Post bijgewerkt. Doorverwijzen…'
 
 );
+
