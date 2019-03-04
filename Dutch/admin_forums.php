c335c2e5fb96c632a278af786c8ec4950ec1d017
fp10Ho <48217340+fp10Ho@users.noreply.github.com>
2019-03-04 17:51:02 +0100

    Update admin_forums.php
    Some typo's changed and some grammar correction

--- Dutch/admin_forums.php
+++ Dutch/admin_forums.php
@@ -1,22 +1,27 @@
 <?php
 
+
 // Taaldefinities gebruikt in admin-forums.php
+
 $lang_admin_forums = array(
 
-'Forum added redirect'		=>	'Forum toegevoegd. Doorverwijzen …',
-'Forum deleted redirect'	=>	'Forum verwijderd. Doorverwijzen …',
-'Forums updated redirect'	=>	'Forums geüpdate. Doorverwijzen …',
-'Forum updated redirect'	=>	'Forum geüpdate. Doorverwijzen …',
-'Perms reverted redirect'	=>	'Permissies hersteld naar standaard. Doorverwijzen …',
+
+'Forum added redirect'		=>	'Forum toegevoegd. Doorverwijzen…',
+'Forum deleted redirect'	=>	'Forum verwijderd. Doorverwijzen…',
+'Forums updated redirect'	=>	'Forums geüpdate. Doorverwijzen…',
+'Forum updated redirect'	=>	'Forum geüpdate. Doorverwijzen…',
+'Perms reverted redirect'	=>	'Permissies hersteld naar standaard. Doorverwijzen…',
 'Must enter name message'	=>	'Je moet een forumnaam opgeven.',
 'Must be integer message'	=>	'Plaats moet een natuurlijk getal zijn (1, 2, 3, etc.).',
 'New forum'					=>	'Nieuw forum',
 
+
 // Pagina
+
 'Add forum head'			=>	'Toevoegen',
 'Create new subhead'		=>	'Maak een nieuw forum',
 'Add forum label'			=>	'Forum toevoegen aan categorie',
-'Add forum help'			=>	'Selecteer de categorie waar je je nieuwe forum wil plaatsen.',
+'Add forum help'			=>	'Selecteer de categorie waar je een nieuw forum wilt plaatsen.',
 'Add forum'					=>	'Forum toevoegen',
 'No categories exist'		=>	'Geen categorieën',
 'Edit forums head'			=>	'Forums bewerken',
@@ -27,11 +32,13 @@ $lang_admin_forums = array(
 'Position label'			=>	'Positie',
 'Update positions'			=>	'Update positie',
 'Confirm delete head'		=>	'Forum verwijderen bevestigen',
-'Confirm delete subhead'	=>	'Belangrijk! Lees dit voordat je een forum verwijderd!',
+'Confirm delete subhead'	=>	'Belangrijk! Lees dit voordat je een forum verwijdert!',
 'Confirm delete info'		=>	'Ben je zeker dat je het forum <strong>%s</strong> wilt verwijderen?',
 'Confirm delete warn'		=>	'<strong>WAARSCHUWING!</strong> Het verwijderen van een forum heeft tot direct gevolg dat ook alle topics en posts in dit forum worden verwijderd',
 
+
 // Detail pagina
+
 'Edit forum head'			=>	'Forum bewerken',
 'Edit details subhead'		=>	'Bewerk forum details',
 'Forum name label'			=>	'Forum naam',
@@ -42,9 +49,9 @@ $lang_admin_forums = array(
 'Topic start'				=>	'Topic start',
 'Subject'					=>	'Onderwerp',
 'Redirect label'			=>	'URL doorverwijzen',
-'Redirect help'				=>	'Alleen mogelijk bij een leef forum',
+'Redirect help'				=>	'Alleen mogelijk bij een leeg forum',
 'Group permissions subhead'	=>	'Bewerkt groepspermissies voor dit forum',
-'Group permissions info'	=>	'Je kan voor dit forum de forum gebaseerde permissies per groep wijzigen. Als je niks veranderd aan de permissies binnen dit forum, zullen de standaardpermissies worden gebruikt. Permissies die verschillen van de wat voor de desbetreffende groep standaard permissies zijn (als aangeduid in %s), worden deze in het rood aangeduid. De "Forum lezen"-permissie checkbox is onzichtbaar voor groepen zonder "Forum lezen" permissie. Voor doorverwijsforums is enkel de "Forum lezen"-permissie bewerkbaar.',
+'Group permissions info'	=>	'Je kan voor dit forum de forum gebaseerde permissies per groep wijzigen. Als je niets verandert aan de permissies binnen dit forum, zullen de standaardpermissies worden gebruikt. Permissies die verschillen van wat voor de desbetreffende groep standaard permissies zijn (als aangeduid in %s), worden in het rood aangeduid. De "Forum lezen"-permissie checkbox is onzichtbaar voor groepen zonder "Forum lezen" permissie. Voor doorverwijsforums is enkel de "Forum lezen"-permissie bewerkbaar.',
 'Read forum label'			=>	'Forum lezen',
 'Post replies label'		=>	'Reageer op topics',
 'Post topics label'			=>	'Maak nieuw topic',
