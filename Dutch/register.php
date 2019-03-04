8ae738386378a78692d7860584f0a2e533e8ca8d
fp10Ho <48217340+fp10Ho@users.noreply.github.com>
2019-03-04 19:02:02 +0100

    Update register.php
    Some words more Dutch and some grammar correction

--- Dutch/register.php
+++ Dutch/register.php
@@ -1,37 +1,42 @@
 <?php
 
 // Taaldefinities gebruikt in register.php
+
 $lang_register = array(
 
 // Diverse
-'No new regs'				=>	'Dit forum accepteerd geen nieuwe leden.',
-'Reg cancel redirect'		=>	'Registratie geannuleerd. Doorverwijzen …',
+
+'No new regs'				=>	'U dient geregistreerd te zijn om te kunnen inloggen op dit forum.',
+'Reg cancel redirect'		=>	'Registratie geannuleerd. Doorverwijzen…',
 'Forum rules'				=>	'Forum regels',
 'Rules legend'				=>	'Je moet de regels accepteren om te registreren',
-'Registration flood'		=>	'Een nieuwe gebruiker is al geregistreerd onder dit IP adres. Je zal een uur moeten wachten voordat je weer opnieuw kan registreren met dit IP.',
+'Registration flood'		=>	'Een nieuwe gebruiker is al geregistreerd onder dit IP-adres. Je zult een uur moeten wachten voordat je opnieuw kunt registreren met dit IP.',
 'Agree'						=>	'Accepteer',
 'Cancel'					=>	'Annuleer',
 'Register'					=>	'Registreer',
 
 // Validatie
+
 'Registration errors'		=>	'Registratie error',
-'Registration errors info'	=>	'De volgende problemen moeten worden opgelost voordat je kan registreren:',
+'Registration errors info'	=>	'De volgende problemen moeten worden opgelost voordat je kunt registreren:',
 'Username censor'			=>	'De gebruikersnaam die je hebt opgegeven mag niet worden gebruikt.',
-'Username dupe 1'			=>	'Deze gebruikersnaam wordt al gebruikt',
-'Username dupe 2'			=>	'De gebruikersnaam die je hebt opgegeven is te kort. Je gebruikersnaam moet minstens een alfanummeriek teken bevatten (a-z, A-Z of 0-9). Kies een andere gebruikersnaam.',
-'Email not match'			=>	'E-mail adres komt niet overeen.',
+'Username dupe 1'			=>	'Deze gebruikersnaam wordt al gebruikt.',
+'Username dupe 2'			=>	'De gebruikersnaam die je hebt opgegeven is te kort. Een gebruikersnaam moet minstens een alfanummeriek teken bevatten (a-z, A-Z of 0-9). Kies een andere gebruikersnaam.',
+'Email not match'			=>	'E-mailadres komt niet overeen.',
 
 // Registration email stuff
-'Reg email'					=>	'Bedankt voor je registratie! Je wachtwoord is naar het opgegeven e-mail adres gestuurd. Als het niet aankomt kan je de forum admin berijken op',
-'Reg complete'				=>	'Registratie voltooid. Je wordt ingelogd en doorverwezen …',
+
+'Reg email'					=>	'Bedankt voor je registratie! Je wachtwoord is naar het opgegeven e-mailadres gestuurd. Als het niet aankomt kan je de forum beheerder bereiken op',
+'Reg complete'				=>	'Registratie voltooid. Je wordt ingelogd en doorverwezen…',
 
 // Register info
-'Desc 1'					=>	'Registreren geeft je toegang tot een aantal mogelijkheden die zonder registratie niet beschikbaar zijn. Zo krijg je de optie om je berichten te bewerken en verwijderen, je eigen sig samen te stellen. Als je vragen hebt kan je deze altijd stellen aan een forum admin of mod.',
-'Desc 2'					=>	'Onderstaande velden moet je invullen om te registreren. Eens je bent geregistreerd kan je je profiel bekijken en je persoonlijke instellingen bewerken. De velden die je hier invuld zijn maar een klein deel van alle mogelijke instellingen!',
+
+'Desc 1'					=>	'Registreren geeft je toegang tot een aantal mogelijkheden die zonder registratie niet beschikbaar zijn. Zo krijg je de optie om je berichten te bewerken en verwijderen, je eigen sig samen te stellen. Als je vragen hebt kan je deze altijd stellen aan de beheerder van het forum.',
+'Desc 2'					=>	'Onderstaande velden moet je invullen om te registreren. Als je geregistreerd bent kan je je profiel bekijken en je persoonlijke instellingen bewerken. De velden die je hier invult zijn maar een klein deel van alle mogelijke instellingen!',
 'Username legend'			=>	'Geef een gebruikersnaam op van minimum 2 tot maximum 25 tekens op',
 'Pass legend'				=>	'Geef een wachtwoord op en bevestig het',
 'Pass info'					=>	'Een wachtwoord moet minstens 4 tekens bevatten',
-'Email info'				=>	'Je moet een correct e-mail adres invullen om het wachtwoord naar te sturen.',
-'Confirm email'				=>	'Bevestig je e-mail adres',
+'Email info'				=>	'Je moet een juist e-mailadres invullen om het wachtwoord naar te sturen.',
+'Confirm email'				=>	'Bevestig je e-mailadres',
 
 );
