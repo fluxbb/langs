8a127ac78e242295c2dad06a3b222d3d8559d8e1
fp10Ho <48217340+fp10Ho@users.noreply.github.com>
2019-03-04 19:09:14 +0100

    Update userlist.php
    Some grammar correction and small addition

--- Dutch/userlist.php
+++ Dutch/userlist.php
@@ -1,10 +1,11 @@
 <?php
 
 // Taaldefinities gebruikt in userlist.php
+
 $lang_ul = array(
 
 'User find legend'	=>	'Zoek en sorteer gebruikers',
-'User search info'	=>	'Geef een gebruikersnaam op om naar te zoeken en/of een gebruikersgroep in op te filteren. Het gebruikersnaam veld kan worden leeg gelaten.',
+'User search info'	=>	'Geef een gebruikersnaam op om naar te zoeken en/of een gebruikersgroep om op te filteren. Het veld van de gebruikersnaam kan worden leeg gelaten. Gebruik de wildcard (*) voor partiĂ«le overeenkomsten.',
 'User sort info'	=>	'Sorteer gebruikers op naam, datum van registratie of aantal posts en op- of aflopend.',
 'User group'		=>	'Gebruikersgroep',
 'No of posts'		=>	'Aantal posts',
