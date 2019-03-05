af0f67d4f39c25c9351c8e20871195f042dc667a
fp10Ho <48217340+fp10Ho@users.noreply.github.com>
2019-03-05 07:51:51 +0100

    Update admin_maintenance.php
    Some grammar correction

--- Dutch/admin_maintenance.php
+++ Dutch/admin_maintenance.php
@@ -1,20 +1,16 @@
 <?php
 
-
-
 // Taaldefinities gebruikt inn admin_maintenance.php
 
 $lang_admin_maintenance = array(
 
-
-
 'Maintenance head'				=>	'Forum onderhoud',
 'Rebuild index subhead'			=>	'Herbouw zoek index',
-'Rebuild index info'			=>	'Als je posts handmatig hebt toegevoegd, bewerkt of verwijderd in de database of als er problemen zijn met het zoeken naar posts, dan kan je de zoek index herbouwen. Voor de beste prestaties kan je best het forum in %s plaatsen tijden dit proces. <strong>Het herbouwen van de zoek index kan lang duren en zal veel van de server vereisen tijdens dit proces!</strong>',
+'Rebuild index info'			=>	'Als je posts handmatig hebt toegevoegd, bewerkt of verwijderd in de database of als er problemen zijn met het zoeken naar posts, dan kun je de zoek index herbouwen. Voor de beste prestaties kan je best het forum in %s plaatsen tijden dit proces. <strong>Het herbouwen van de zoek index kan lang duren en zal veel van de server vereisen tijdens dit proces!</strong>',
 'Posts per cycle label'			=>	'Posts per keer',
 'Posts per cycle help'			=>	'Het aantal posts dat kan worden gedaan per pagina weergaven. Bv. als je deze instelling op 300 (standaard) zet, zullen 300 berichten worden gebruikt om de zoek index opnieuw op te bouwen, vervolgens wordt de pagina herladen voor de volgende 300. Dit voorkomt time-outs tijdens het proces.',
 'Starting post label'			=>	'Start post ID',
-'Starting post help'			=>	'Het ID van de post waar de herbouw van de zoek index moet beginnen. De standaard waarde is het eerste beschikbare ID in de database, het aanpassen van deze instelling is niet nodig.',
+'Starting post help'			=>	'Het ID van de post waar de herbouw van de zoek index moet beginnen. De standaard waarde is het eerst beschikbare ID in de database, het aanpassen van deze instelling is niet nodig.',
 'Empty index label'				=>	'Verwijder index',
 'Empty index help'				=>	'Selecteer dit als je wilt dat de huidige zoek index wordt verwijderd voor het herbouwen ervan begint (zie hieronder).',
 'Rebuild completed info'		=>	'Als dit proces klaar is wordt je automatisch terug gestuurd naar deze pagina. Het wordt aangeraden om JavaScript aan te zetten voor je browser tijdens dit proces (zo kan de pagina worden herladen als een cycli klaar is). Als het proces wordt onderbroken, onthoud dan het ID van de post die als laatst werd gebruikt. Later kan je dan dit ID+1 invoeren in het veld "Start post ID" om verder te gaan (Let wel op dat "Verwijder index" uit staat!).',
@@ -40,8 +36,6 @@ $lang_admin_maintenance = array(
 'Prune info'					=>	'Gebruik deze functie voorzichtig! <strong>Opgeruimde posts kunnen niet worden hersteld.</strong> Voor de beste prestaties kan je het forum in %s zetten tijdens het proces.',
 'Confirm prune subhead'			=>	'Bevestig posts opruimen.',
 'Confirm prune info'			=>	'Ben je zeker dat je alle topics ouder dan %s dagen van %s (%s topics) wilt opruimen?',
-'Confirm prune warn'			=>	'WAARSCHUWING! Posts opruimen verwijderd alle in aanmerking komende posts permanent.',
-
+'Confirm prune warn'			=>	'WAARSCHUWING! Posts opruimen verwijdert alle in aanmerking komende posts permanent.',
 
 );
-
