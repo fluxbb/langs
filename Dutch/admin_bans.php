053c1738bb52bd82d1d39fa15db917352cd81466
fp10Ho <48217340+fp10Ho@users.noreply.github.com>
2019-03-05 08:51:01 +0100

    Update admin_bans.php
    Removed empty lines

--- Dutch/admin_bans.php
+++ Dutch/admin_bans.php
@@ -1,12 +1,7 @@
 <?php
 
-
-
 // Taaldefinities gebruikt in admin_bans.php
-
 $lang_admin_bans = array(
-
-
 
 'No user message'			=>	'Er is geen gebruiker met deze gebruikersnaam. Als je een ban wilt instellen die niet gerelateerd is aan geen gebruikersnaam laat je dit veld leeg.',
 'No user ID message'		=>	'Er is geen gebruiker met dit ID.',
@@ -19,17 +14,14 @@ $lang_admin_bans = array(
 'Invalid date message'		=>	'Je hebt een ongeldige vervaldatum opgegeven.',
 'Invalid date reasons'		=>	'Het formaat van een datum is JJJJ-MM-DD en moet minsens 1 dag later zijn.',
 'Ban added redirect'		=>	'Ban toegevoegd. Doorverwijzen …' ,
-
 'Ban edited redirect'		=>	'Ban bewerkt. Doorverwijzen …',
 'Ban removed redirect'		=>	'Ban verwijderd. Doorverwijzen …',
-
 
 'New ban head'				=>	'Nieuwe ban',
 'Add ban subhead'			=>	'Ban toevoegen',
 'Username label'			=>	'Gebruikersnaam',
 'Username help'				=>	'De gebruiker die je wilt verbannen (niet-hoofdlettergevoelig).',
 'Username advanced help'	=>	'De gebruiker die je wilt verbannen (niet-hoofdlettergevoelig). Op de volgende pagina kan je een IP adres of e-mail opgeven om ook te verbannen, laat die velden leeg als je dat niet wilt.',
-
 
 'Ban search head'			=>	'Ban zoeken',
 'Ban search subhead'		=>	'Geef zoekopdracht in',
@@ -47,7 +39,6 @@ $lang_admin_bans = array(
 'Descending'				=>	'Aflopend',
 'Submit search'				=>	'Zoeken',
 
-
 'E-mail label'				=>	'E-mail',
 'E-mail help'				=>	'Het e-mailadres of e-mail domein dat je wilt verbannen (bv. naam@domein.be of domein.be). Bekijk "Verboden e-mailadressen toestaan" in Permissies voor meer info.',
 'IP label'					=>	'IP adres',
@@ -62,7 +53,6 @@ $lang_admin_bans = array(
 'Expire date label'			=>	'Vervaldatum',
 'Expire date help'			=>	'De datum waarop de ban automatisch vervalt (formaat: jjjj-mm-dd). Voer geen datum in om de ban handmatig te verwijderen.',
 
-
 'Results head'				=>	'Zoek resultaten',
 'Results username head'		=>	'Gebruikersnaam',
 'Results e-mail head'		=>	'E-mail',
@@ -74,6 +64,4 @@ $lang_admin_bans = array(
 'No match'					=>	'Geen resultaat',
 'Unknown'					=>	'Onbekend',
 
-
 );
-
