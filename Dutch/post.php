1d42ae5b3ed5caf624351499c24dbd391087a6a5
fp10Ho <48217340+fp10Ho@users.noreply.github.com>
2019-03-06 13:59:50 +0100

    Update post.php
    Some different wording, some grammar correction

--- Dutch/post.php
+++ Dutch/post.php
@@ -4,14 +4,14 @@
 $lang_post = array(
 
 // Bericht valideren
-'No subject'		=>	'Je moet een onderwerp opgeven.',
-'No subject after censoring'	=>	'Je moet een onderwerp opgeven. Door de filter werd je onderwerp verwijderd.',
-'Too long subject'	=>	'Onderwerp mag maximum 70 karakters bevatten.',
+'No subject'		=>	'Vul een onderwerp in.',
+'No subject after censoring'	=>	'Vul een ander onderwerp in. Door de filter werd je onderwerp verwijderd.',
+'Too long subject'	=>	'Onderwerp mag maximaal 70 karakters bevatten.',
 'No message'		=>	'Je moet een bericht opgeven.',
 'No message after censoring'	=>	'Je moet een bericht opgeven. Door de filter werd je bericht verwijderd',
 'Too long message'	=>	'Je bericht mag niet langer zijn dan %s bytes.',
 'All caps subject'	=>	'Onderwerp kan niet bestaan uit enkel hoofdletters.',
-'All caps message'	=>	'Bericht kan niet bestaan uit enkel hoofdletters..',
+'All caps message'	=>	'Bericht kan niet bestaan uit enkel hoofdletters.',
 'Empty after strip'	=>	'Het lijkt erop dat je post enkel bestaat uit BBCodes. Dit kan gebeuren als je citaten gebruikt die het diepte niveau overschrijden',
 
 // Posten
@@ -19,7 +19,8 @@ $lang_post = array(
 'Post errors info'	=>	'De volgende problemen moeten worden opgelost voordat je post kan worden gepubliceerd:',
 'Post preview'		=>	'Toon voorbeeld',
 'Guest name'		=>	'Naam', // Enkel voor gasten
-'Post redirect'		=>	'Post ingevoerd. Doorverwijzen …',
+
+'Post redirect'		=>	'Post ingevoerd. Doorverwijzen…',
 'Post a reply'		=>	'Reageren',
 'Post new topic'	=>	'Nieuw topic',
 'Hide smilies'		=>	'Gebruik nooit emoticons in deze post',
@@ -34,6 +35,6 @@ $lang_post = array(
 'Edit post legend'	=>	'Bewerken en verzenden',
 'Silent edit'		=>	'Stil bewerken (weergeef "Bewerkt door ..." niet in dit topic)',
 'Edit post'			=>	'Bewerk post',
-'Edit redirect'		=>	'Post bijgewerkt. Doorverwijzen …'
+'Edit redirect'		=>	'Post bijgewerkt. Doorverwijzen…'
 
 );
